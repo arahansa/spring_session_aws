f364d8b2ce391ea0db112eeb28fa97828879122d
arahansa <arahansa@naver.com>
2017-02-19 22:38:55 +0900

    first commit

---
+++ src/main/java/com/arahansa/SpringsessionApplication.java
@@ -0,0 +1,12 @@
+package com.arahansa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsessionApplication.class, args);
+	}
+}
